6dd24d17a12e4ebc94068e927130d1590a5ffbcc
JASPRET <jaspret@JASPRETs-iMac.local>
2015-09-19 19:01:32 +0530

    Initial Commit

---
+++ deliverTests/deliverTests.swift
@@ -0,0 +1,36 @@
+//
+//  deliverTests.swift
+//  deliverTests
+//
+//  Created by JASPRET on 19/09/15.
+//  Copyright (c) 2015 JASPRET. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class deliverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
