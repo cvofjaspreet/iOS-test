502948e4d800dfa4cbc9712cc050d5ab69d6f722
JASPRET <jaspret@JASPRETs-iMac.local>
2015-09-25 00:09:19 +0530

    working on country list

--- deliver/SelectCountryCode.swift
+++ deliver/SelectCountryCode.swift
@@ -9,22 +9,49 @@
 import Foundation
 import UIKit
 
-class SelectCountryCode: UIViewController{
+class SelectCountryCode: UITableViewController,UITableViewDataSource{
     
     @IBOutlet weak var countryList: UITableView!
     
+    var names: NSDictionary!
     
     
     override func viewDidLoad() {
       self.title="Select Your Country"
         
+        loadCountries();
         
     }
     
+    func loadCountries(){
+        
+        if let path = NSBundle.mainBundle().pathForResource("CountryList", ofType: "plist") {
+            names = NSDictionary(contentsOfFile: path)
+        }
+        if let dict = names {
+            var keys=names.allKeys
+            
+            println(keys)
+        }
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
        
     }
     
+   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
     
+   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+ 
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as! UITableViewCell
+       
+        return cell
+    }
+ 
 }
