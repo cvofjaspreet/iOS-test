5e2c093911d160d6b66c678a2f553a8cafea9256
cvofjaspreet <jaspret@jasprets-imac.local>
2015-10-03 21:15:07 +0530

    working on collection view

---
+++ deliver/BaseView.swift
@@ -0,0 +1,14 @@
+//
+//  BaseView.swift
+//  deliver
+//
+//  Created by JASPRET on 03/10/15.
+//  Copyright © 2015 JASPRET. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseView: UICollectionViewController{
+
+}
